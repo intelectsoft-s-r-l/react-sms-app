b626c3c300ecb94997fe439345013d3493335440
dipzera <nathanielthomaz@gmail.com>
2021-03-31 14:04:06 +0300

    Inserting the token upon redirecting inside cookie

--- src/views/auth-views/authentication/validate/index.tsx
+++ src/views/auth-views/authentication/validate/index.tsx
@@ -1,4 +1,4 @@
-import { APP_PREFIX_PATH, AUTH_PREFIX_PATH } from "configs/AppConfig";
+import { APP_PREFIX_PATH, AUTH_PREFIX_PATH, DOMAIN } from "configs/AppConfig";
 import * as React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,17 +7,21 @@ import { getProfileInfo } from "redux/actions/Account";
 import { authenticated } from "redux/actions/Auth";
 import { IState } from "redux/reducers";
 import { useQuery } from "utils/hooks/useQuery";
+import Cookies from "js-cookie";
 
 function Validate(props: RouteComponentProps) {
   const dispatch = useDispatch();
   const query = useQuery();
-  const Token = useSelector((state: IState) => state.auth?.token);
+
   useEffect(() => {
     try {
-      if (query.get("id")) {
-        dispatch(authenticated(query.get("id")!.toString()));
-        dispatch(getProfileInfo());
-        if (Token) props.history.push(APP_PREFIX_PATH);
+      if (query.get("token")) {
+        Cookies.set("Token", query.get("token")!.toString(), {
+          expires: 1,
+          domain: DOMAIN,
+          path: "/",
+        });
+        props.history.push(APP_PREFIX_PATH);
       }
     } catch {
       props.history.push(AUTH_PREFIX_PATH);
